2a66fde954a973e787d31f0d32a79268fd6df3e8
GennadiySerikov <serikov.gen@yandex.ru>
2022-11-29 19:00:07 +0300

    add lesson 1 empty tasks

---
+++ homework.java
@@ -0,0 +1,5 @@
+public class homework {
+    public static void main(String[] args) {       
+        System.out.println(lesson1.triangle_num.execute(10));
+    }
+}
---
+++ lesson1/triangle_num.java
@@ -0,0 +1,7 @@
+package lesson1;
+
+public class triangle_num {
+    public static int execute(int digit) {
+        return 0;
+    }
+}
