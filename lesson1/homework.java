ea2377acbf4294915164d8e998c89602fe65ab49
Gena Serikov <serikov.gen@yandex.ru>
2023-02-12 20:29:12 +0300

    feat: primes method

--- lesson1/homework.java
+++ lesson1/homework.java
@@ -1,8 +1,19 @@
 package lesson1;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class homework {
     public static void main(String[] args) {
+       System.out.println(GetTriangleNumber(4));
        System.out.println(GetFactorial(3));
+
+       for (Integer prime : GetPrimesTo(1000)) {
+            System.out.printf("%d ", prime);
+       }
     }
 
     public static int GetTriangleNumber(int bound) 
@@ -15,5 +26,37 @@ public class homework {
         if(number <= 1)
             return 1;
         return number * GetFactorial(number - 1);
+    }
+
+    // public static List<Integer> GetPhrimesTo(int n) {
+    //     return IntStream.rangeClosed(2, n)
+    //       .filter(x -> isPrime(x)).boxed()
+    //       .collect(Collectors.toList());
+    // }
+    // private static boolean isPrime(int number) {
+    //     return IntStream.rangeClosed(2, (int) (Math.sqrt(number)))
+    //       .allMatch(n -> number % n != 0);
+    // }
+
+    public static List<Integer> GetPrimesTo(int number)
+    {
+        var primes = new ArrayList<>(Arrays.asList(1, 2, 3));
+              
+        for (int i = 4; i < number; i++) {
+            int bound = (int)(Math.sqrt((double)i));
+            boolean isPrime = true;
+            for (Integer prime : primes) {
+                if(prime != 1 && prime <= bound && i % prime == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+            {
+                primes.add(i);
+            }
+        }
+        return primes;
     }
 }
