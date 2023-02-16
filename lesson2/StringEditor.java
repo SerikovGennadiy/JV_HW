5a6b3aadc46cc233611afde149fd71146bbb71e3
Gena Serikov <serikov.gen@yandex.ru>
2023-02-16 11:42:52 +0300

    feat: lesson2 complite. string editor

---
+++ lesson2/StringEditor.java
@@ -0,0 +1,40 @@
+package lesson2;
+
+import java.util.regex.Pattern;
+
+public class StringEditor {
+    private String _str = null;
+    public StringEditor(String str) throws Exception
+    {
+        if(str == null || str.trim().isEmpty())
+            throw new Exception(String.format("%s is null or empty", str.getClass().getName()));
+        _str = str;
+    }
+    
+    public StringEditor removeSpaces()
+    {
+        _str = _str.replaceAll("\\s+", " ");
+        return this;
+    }
+
+    public StringEditor dotInEnd()
+    {
+        _str = _str + ".";
+        return this;
+    }
+
+    public StringEditor editCapitals()
+    {
+        _str = Pattern
+                .compile("(\\s+)([A-ZА-Я])")
+                .matcher(_str)
+                .replaceAll(gr -> ".\s" + gr.group(2));
+        return this;
+    }
+
+    public String getResult()
+    {
+        return this._str;
+    }
+   
+}
